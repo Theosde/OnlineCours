8ce6cd6e6701c29b0f69845e35bdb500281a79b4
Theo <theo.lassiaz@sfr.fr>
2019-12-16 17:52:17 +0100

    fonctionnalité filter + pagination

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,39 +1,74 @@
-import React, { useState }from 'react'
-import MenuLanguagesDeroulants from './MenuLanguagesDeroulants'
-import MenuPrixDeroulants from './MenuPrixDeroulants'
+import React, { useState } from "react";
+import MenuLanguagesDeroulants from "./MenuLanguagesDeroulants";
+import MenuPrixDeroulants from "./MenuPrixDeroulants";
 
-import './DetailsCours.css'
+import "./DetailsCours.css";
 
-function FilterBar() {
+function FilterBar(props) {
+  const [showMenu, setShowMenu] = useState({
+    show: false
+  });
+  const [showMenuPrice, setShowMenuPrice] = useState({
+    show: false
+  });
 
-    const [showMenu, setShowMenu] = useState({
-        show:false
-      });
-      const [showMenuPrice, setShowMenuPrice] = useState({
-        show:false
-      });
+  const [inputValue, setInputValue] = useState("");
 
-      var handleClick = e => {
-        e.preventDefault();
-        var copyState = { ...showMenu };
-              copyState.show = !showMenu.show;
-              setShowMenu(copyState);
-      };
+  var handleClick = e => {
+    e.preventDefault();
+    var copyState = { ...showMenu };
+    copyState.show = !showMenu.show;
+    setShowMenu(copyState);
+  };
 
-      var handleClick2 = e => {
-        e.preventDefault();
-        var copyState = { ...showMenuPrice };
-              copyState.show = !showMenuPrice.show;
-              setShowMenuPrice(copyState);
-      };
+  var handleClick2 = e => {
+    e.preventDefault();
+    var copyState = { ...showMenuPrice };
+    copyState.show = !showMenuPrice.show;
+    setShowMenuPrice(copyState);
+  };
 
-    return (
-        <div className='filterBar'>
-            {showMenu.show === false ?<div className='langageFilter' onClick={handleClick}>CATEGORIES</div>:<MenuLanguagesDeroulants/>}
-            {showMenuPrice.show === false ?<div className='priceFilter'onClick={handleClick2}>PRIX</div>:<MenuPrixDeroulants/>}
-            <input className='searchFilter' type='text' placeholder='Entrer le nom du cours ici'/>
+  var onChangeInputValue = e => {
+    setInputValue(e.target.value);
+    props.handleChangeinputParent(e.target.value);
+  };
+
+  var onChangePrixValue = e => {
+    props.handleChangeDeroulantPriceParent(e);
+  };
+
+  var onChangeCategorieValue = e => {
+    props.handleChangeDeroulantCategorieParent(e);
+  };
+
+  return (
+    <div className="filterBar">
+      {showMenu.show === false ? (
+        <div className="langageFilter" onClick={handleClick}>
+          CATEGORIES
         </div>
-    )
+      ) : (
+        <MenuLanguagesDeroulants
+          listeAllCategorie={props.allCategorieParent}
+          onChangeCategorieValueFilterBar={onChangeCategorieValue}
+        />
+      )}
+      {showMenuPrice.show === false ? (
+        <div className="priceFilter" onClick={handleClick2}>
+          PRIX
+        </div>
+      ) : (
+        <MenuPrixDeroulants onChangePrixValueFilterBar={onChangePrixValue} />
+      )}
+      <input
+        className="searchFilter"
+        type="text"
+        value={inputValue}
+        onChange={e => onChangeInputValue(e)}
+        placeholder="Entrer le nom du cours ici"
+      />
+    </div>
+  );
 }
 
-export default FilterBar
+export default FilterBar;
