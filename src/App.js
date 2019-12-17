6c3dbf74a9a656b070434731d20b0b69c0d75d69
Arnaud Rey <arnaud.rey.job@gmail.com>
2019-12-17 17:59:37 +0100

    'responsive+dashboardHomePage'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import "./index.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import Navbar from "./components/NavBar";
-import Home from "./components/Home";
-import Error404 from "./components/Error404";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import ListeCours from "./components/ListeCours";
-import MonCompte from "./components/MonCompte";
-import DashBoard from "./components/DashBoard";
-import DashBoardAdmin from "./components/DashboardAdmin";
-import DetailsCours from "./components/DetailsCours";
-import DashBoardMenu from "./components/DashBoardMenu";
+import Navbar from "./components/NavBar"
+import Home from "./components/Home"
+import Error404 from "./components/Error404"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import ListeCours from "./components/ListeCours"
+import MonCompte from "./components/MonCompte"
+import DashBoard from "./components/DashBoard"
+import DashBoardAdmin from "./components/DashboardAdmin"
+import DetailsCours from "./components/DetailsCours"
+import DashBoardMenu from "./components/DashBoardMenu"
+import VentesHistoric from './components/VentesHistoric'
 
 class App extends Component {
   render() {
@@ -31,6 +32,8 @@ class App extends Component {
             />
             <Route path="/MonCompte" component={MonCompte} />
             <Route path="/Dashboard" component={DashBoardMenu} />
+            <Route path="/add-cours" component={DashBoardAdmin}/>
+            <Route path ="/Ventes-historic" component={VentesHistoric}/>
             <Route path="/Error404" component={Error404} />
             <Redirect to="/Error404" />
           </Switch>
--- src/components/DashBoardMenu.js
+++ src/components/DashBoardMenu.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
+import './DashBoardMenu.css'
 
 function DashBoardMenu() {
     return (
         <div className="background-bg">
             <div className='center-form-myAccount'>
-
+                <div className='ligne-1-panel'>
+                <Link to="/add-cours">
+                    <div class="module">
+                        <img src='../newcours.png'></img>
+                        <p>Créer un nouveau cours</p>
+                    </div>
+                </Link>
+                    <div class="module">
+                        <img src='../cours.png'></img>
+                        <p>Modifier un cours existant</p>
+                    </div>
+                </div>
+                <div className='ligne-2-panel'>
+                <Link to="/Ventes-historic">
+                    <div class="module">
+                        <img src='../income.png'></img>
+                        <p>Consulter les ventes de cours</p>
+                    </div>
+                </Link>
+                    <div class="module">
+                        <img src='../stats.png'></img>
+                        <p>Consulter les statistiques</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
